defe5d69490fe5ece2f6f47d6ad3d790a7551383
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2022-01-02 16:25:10 +0200

    changes

--- server/client/src/Components/Progress.js
+++ server/client/src/Components/Progress.js
@@ -72,7 +72,7 @@ function Progress() {
 
     fetchData();
 
-  },[])
+  },[item])
   return (
     <div id="progress" className="progress-container">
         <form className="workout-details-form" onSubmit= {handleSubmit}   >
